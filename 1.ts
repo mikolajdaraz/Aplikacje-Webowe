0a441db4b73bbce766f6c1d85ebc7f94b94daa1c
Mikolaj <mikolajdara@gmail.com>
2021-03-05 20:04:56 +0100

    03.05.2021

---
+++ 1.ts
@@ -0,0 +1,22 @@
+const czlowiek = {
+    imie:'',
+    wiek: '',
+    nazwisko: ''
+}
+//w typecsript nie da się tak zrobić jak js 
+//czlowiek.nazw = 'asd'
+
+czlowiek.nazwisko = 'ad'
+//let a  = 10
+//a = 'aaaa'  //w js to jest poprawne, w TypeScripcie nie 
+
+let a: any  = 10
+a = 'aaa'
+
+function fetchData(data: any){
+
+}
+
+//instalacj npm:
+//npm i -g typescript
+
